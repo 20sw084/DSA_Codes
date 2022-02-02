adafe3ea01744a46a82a995fa1c20e4d214b1629
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-02-02 21:52:04 +0500

    Update ArrayStack.java

--- Lec#10/ArrayStack.java
+++ Lec#10/ArrayStack.java
@@ -52,35 +52,7 @@ public class ArrayStack implements Stack {
 		}
 		a=copy;
 	}
-	
-	public List toLinkedList() {
-		List list=new List(a[0]);
-		List listCopy=list;
-		for(int i=1;i<a.length;i++) {
-			listCopy.next=new List(a[i]);
-			listCopy=listCopy.next;
-		}
-		return list;
-	}
-	
-	public LinkedListStack1 toLinkedStack() {
-		LinkedListStack1 lts=new LinkedListStack1(a[0]);
-		LinkedListStack1 ltsCopy=lts;
-		for(int i=1;i<a.length;i++) {
-			lts.next=new LinkedListStack1(a[i]);	
-			lts=lts.next;
-		}
-		return ltsCopy;
-	}
-	
-	public String toString() {
-		String str="Element 1:\t"+a[0];
-		for(int i=1;i<a.length;i++) {
-			str+="\nElement "+(i+1)+":\t"+a[i];
-		}
-		return str;
-	}
-	
+		
 	public void printStack() {
 		for(int i=0;i<a.length;i++) 
 			System.out.println(a[i]);
@@ -100,19 +72,6 @@ public class ArrayStack implements Stack {
 		as.push("Strawberry");
 //		System.out.println("Size of Array is: "+as.size());
 //		System.out.println(as.peek());
-		
-//		List list=as.toLinkedList();
-//		
-//		for(List i=list;i!=null;i=i.next) {
-//			System.out.println(i.data);
-//		}
-		
-//		LinkedListStack1 lts=as.toLinkedStack();
-//		
-//		for(LinkedListStack1 i=lts;i!=null;i=i.next) {
-//			System.out.println(i.data);
-//		}
-		System.out.println(as.toString());	
 		
 	}
 }
