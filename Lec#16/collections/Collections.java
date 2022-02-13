2c43a25c670b6029bbef042566bc4467e97a26b2
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-02-13 12:26:38 +0500

    Add files via upload

---
+++ Lec#16/collections/Collections.java
@@ -0,0 +1,11 @@
+package collections;
+	
+public interface Collections {
+	public boolean add(Object o);
+	public void clear();
+	public boolean contains(Object o);
+	public boolean isEmpty();
+	public Iterator iterator();
+	public boolean remove(Object o);
+	public int size();
+}
