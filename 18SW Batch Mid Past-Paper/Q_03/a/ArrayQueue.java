55376ef722a31ba1492a6d4f55b53b23c0b04150
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-02-25 06:00:55 -0800

    Add files via upload

---
+++ 18SW Batch Mid Past-Paper/Q_03/a/ArrayQueue.java
@@ -0,0 +1,239 @@
+public class ArrayQueue implements Queue{
+	Object[] data;
+	int size;
+	public ArrayQueue(Object[] data,int size) {
+		super();
+		this.size=size;
+		this.data = data;
+	}
+
+	public ArrayQueue() {
+		super();
+		// TODO Auto-generated constructor stub
+		this.size=5;
+		this.data=new Object[10];
+	}
+
+	public ArrayQueue(Object[] obj) {
+		// TODO Auto-generated constructor stub
+		this.data=obj;
+		this.size=0;
+	}
+	
+	public void resize() {
+		Object[] copy=new Object[data.length];
+		for(int i=0;i<copy.length;i++) {
+			copy[i]=data[i];
+		}
+		data=new Object[copy.length*2];
+		for(int i=0;i<copy.length;i++) {
+			data[i]=copy[i];
+		}
+	}
+
+	public void display() {
+		for(int i=0;i<this.data.length;i++) {
+			if(data[i]!=null)
+			System.out.println(data[i]);
+		}
+	}
+	
+	@Override
+	public void add(Object obj) {
+		if(size<=data.length) {
+			resize();
+		}
+		boolean added=false;
+		if(size==0 || data[0]==null) {
+			data[0]=obj;
+			size++;
+			added=true;
+		}
+		if(added==false) {
+			for(int i=0;i<size;i++) {
+				if(data[i+1]==null) {
+					data[i+1]=obj;
+					this.size++;
+					break;
+				}
+			}
+		}
+	}
+
+	@Override
+	public Object first() {
+		// TODO Auto-generated method stub
+		return data[0];
+	}
+
+	@Override
+	public Object remove() {
+		// TODO Auto-generated method stub
+		if(size==0) {
+			throw new IllegalArgumentException();
+		}
+		Object res=data[0];
+		for(int i=0;i<size ;i++) {
+			data[i]=data[i+1];
+		}
+		size--;
+		return res;
+	}
+
+	public String toString() {
+		String str="";
+		for(int i=0;i<size;i++) {
+			if(data[i]!=null)
+			str+=data[i]+".\t";
+		}
+		return str;
+	}
+	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return size;
+	}
+	
+	public boolean equals(Object o) {
+		boolean res=false;
+		for(int i=0;i<this.data.length;i++) {
+			if(data[i]==o && data[i]!=null) {
+				res= true;
+			}
+		}
+		return res;
+	}
+	
+	public ArrayQueue clone() {
+		ArrayQueue cloned=new ArrayQueue();
+		if(this.size==0) {
+			throw new IllegalArgumentException();
+		}
+		else {
+			cloned.size=1;
+			for(int i=0;i<this.size;i++) {
+				cloned.data[i]=data[i];
+				if(data[i+1]!=null)
+				cloned.size++;
+			}
+		}
+		return cloned;
+	}
+	
+	public void removeBottomElement() {
+		for(int i=0;i<this.data.length;i++) {
+			if(data[i+1]==null) {
+				this.data[i]=null;
+				this.size--;
+				break;
+			}
+		}
+	}
+	
+	public void reverse() {
+		ArrayQueue aq=new ArrayQueue();
+		aq.size=this.size;
+		for(int i=0;i<this.size;i++) {
+			aq.data[i]=this.data[i];
+		}
+		int index=0;
+		for(int i=this.size-1;i>=0;i--) {
+			this.data[index]=aq.data[i];
+			index++;
+		}
+	}
+	
+	public ArrayStack toStack() {
+		ArrayStack as=new ArrayStack();
+		as.a=this.data;
+		as.size=this.size;
+		return as;
+	}
+	
+	public LinkedListQueue toLinkedQueue() {
+		LinkedListQueue llq=new LinkedListQueue();
+		for(int i=0;i<this.data.length;i++) {
+			if(data[i]!=null)
+			llq.add(data[i]);
+		}
+		return llq;
+	}
+	
+	public void deQueue(Object obj,String end) {
+		if(end.equalsIgnoreCase("rear")) {
+		if(size<=data.length) {
+			resize();
+		}
+		boolean added=false;
+		if(size==0 || data[0]==null) {
+			data[0]=obj;
+			size++;
+			added=true;
+		}
+		if(added==false) {
+			for(int i=0;i<size;i++) {
+				if(data[i+1]==null) {
+					data[i+1]=obj;
+					this.size++;
+					break;
+				}
+			}
+		}
+		}
+		else if(end.equalsIgnoreCase("front")) {
+			if(size<=data.length) {
+				resize();
+			}
+			boolean added=false;
+			if(size==0 || data[0]==null) {
+				data[0]=obj;
+				size++;
+				added=true;
+			}
+			if(added==false) {
+				ArrayQueue a=new ArrayQueue();
+				a.add(obj);
+				for(int i=0;i<size;i++) {
+						a.add(data[i]);	
+				}
+				this.data=a.data;
+				this.size++;
+			}
+		}
+		else {
+			System.out.println("In De-Queue, You can insert elements only on rear and front end.");
+		}
+	}
+	
+	public static void main(String[] args) {
+		ArrayQueue aq=new ArrayQueue(new Object[1]);
+		aq.add(15);
+		aq.add(30);
+		aq.add(45);
+		aq.add(60);
+		aq.add(75);
+		aq.add(90);
+		aq.remove();
+		System.out.println("After remove() first element becomes:  "+aq.first());
+		System.out.println("Size of Queue is:  "+aq.size());
+		aq.add(84);
+		System.out.println("Queue after adding element 84 is:  "+aq);
+		System.out.println("Is Queue have Elemene 90??  "+aq.equals(90));
+		aq.removeBottomElement();
+		System.out.println("After Removing Bottom Element is:  "+aq);
+		aq.reverse();
+		System.out.println("After Reversing Queue is:  "+aq);
+		System.out.println("Printing Array Queue:  ");
+		aq.display();
+		LinkedListQueue lq=aq.toLinkedQueue();
+		System.out.println("To Linked Queue is:  "+lq.toString());
+		aq.deQueue(10, "front");
+		System.out.println("After deQueuing Element 10 on front is :  "+aq);
+		ArrayStack as=aq.toStack();
+		System.out.println("To Array Stack is :  "+as);
+		System.out.println("Peek Element of Stack is :  "+as.peek());
+		ArrayQueue a=aq.clone();
+		System.out.println("Cloned Queue is :  "+a.toString());
+	}	
+}
---
+++ 18SW Batch Mid Past-Paper/Q_03/a/LinkedListQueue.java
@@ -0,0 +1,116 @@
+public class LinkedListQueue implements Queue{
+	Node head=new Node();
+	private int size;
+		
+	public static class Node {
+		Object data;	
+		Node next=this;
+		Node prev=this;
+		Node(Object data){
+			this.data=data;
+		}	
+		public Node() {
+			// TODO Auto-generated constructor stub
+		}
+		Node(Object data, Node next , Node prev)
+		{
+			this.data=data;
+			this.next=next;
+			this.prev=prev;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedListQueue llq=new LinkedListQueue();
+		llq.add("One");
+		llq.add("Two");
+		llq.add("Three");
+		llq.add("Four");
+		llq.add("Five");
+		
+		llq.print();
+		System.out.println(llq.toArrayQueue());
+		llq.reverse();
+		System.out.println(llq);
+	}
+	
+		@Override
+		public void add(Object obj) {
+			if(this.head.next==null) {
+				head.next.data=obj;
+				head.prev=head.next;
+				++size;
+			}
+			else {
+				head.prev=head.prev.next=new Node(obj,head,head.prev);
+				++size;
+			}
+		}
+	
+		@Override
+		public Object first() {
+			// TODO Auto-generated method stub
+			if(head.next==null) 
+				throw new IllegalArgumentException("Stack is Empty.");
+			return head.next.data;
+		}
+		
+		public void reverse() {
+			LinkedListQueue llq=new LinkedListQueue();
+			for(Node i=this.head.prev;i!=this.head;i=i.prev) {
+	//			llq.head.next=llq.head.next.next = 
+	//					new Node(i.data,llq.head.next,llq.head.prev);
+				llq.add(i.data);
+			}
+			this.head=llq.head;
+		}
+		
+		@Override
+		public Object remove() {
+			// TODO Auto-generated method stub
+			Node n=null;
+			if(head.next==null) {
+				throw new IllegalArgumentException("Stack is Empty");
+			}
+			else {		
+				n=head.next;
+				head.next=head.next.next;
+				size--;
+			}
+			return n.data;
+		}
+		
+		public String toString() {
+			String str=" ";
+			if(head.next==null) 
+			throw new IllegalArgumentException("Stack is Empty.");
+			for(Node i=this.head.next;i!=head.prev.next;i=i.next) {
+				str+=(i.data+".     ");
+			}
+			return str;
+		}
+		
+		public void print() {
+			if(head.next==null) 
+			throw new IllegalArgumentException("Stack is Empty.");
+			for(Node i=this.head.next;i!=head.prev.next;i=i.next) {
+				System.out.print(i.data+".     ");
+			}
+			System.out.println();
+		}
+		
+		@Override
+		public int size() {
+			// TODO Auto-generated method stub
+			return size;
+		}
+		
+		public ArrayQueue toArrayQueue() {
+			ArrayQueue as=new ArrayQueue();
+			for(Node i=this.head.next;i!=head.prev.next;i=i.next) {
+				as.add(i.data);
+			}
+			return as;
+		}		
+	}
