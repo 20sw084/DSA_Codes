be79c0853e70abbc50dd5bf747cca5ef3a5a4da3
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-02-27 21:35:55 +0500

    Update Node1.java

--- Lec#06/Node1.java
+++ Lec#06/Node1.java
@@ -1,25 +1,28 @@
-package example;
 public class Node1 {
 	int data;
-	Node next;
+	Node1 next;
 
 	public Node1(int data) {
 		this.data=data;
 	}
-	public Node1(int data,Node next) {
+	public Node1(int data,Node1 next) {
 		this(data);
 		this.next=next;
 	}
 	
-	public static void insert(Node1 node,int data) {
-		Node1 n=node;
-		while(n.next!=null) {
-			if(n.next.data>data) {
-				break;
+	public static void insert(Node1 node,int data) { 
+		// Inserts data in sorted form
+		Node1 n=node;  // Keep copy of node 
+		while(n.next!=null) {	// Traverse the whole node
+			if(n.next.data>data) {		// Compares from second data, 
+										// if greater than inserted data.
+				break;					// break the loop
 			}
-			n=n.next;
+			n=n.next;		// Try changing reference of node.
 		}
-		n.next=new Node1(data,n.next);
+		n.next=new Node1(data,n.next);	// Inserts the data at that position.
+										// If data is greater than the whole node,
+										// inserts at last position.
 	}
 	
 	public static void main(String[] args) {
