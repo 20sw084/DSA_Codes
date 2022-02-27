6ec97de34ad617e2e2c0bb1d82fb2ba2d25d2d70
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-02-27 17:43:51 +0500

    Update Resizing_In_Arrays.java

--- Lec#03/Resizing_In_Arrays.java
+++ Lec#03/Resizing_In_Arrays.java
@@ -1,15 +1,13 @@
- * Write down a program of Resizing an array.
+ /* Write down a program of Resizing an array.
  * (In our case, we are going to double it.)
  */
-package dsaCodes;
-
 public class Resizing_In_Arrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				
 		int size=17;
-		int[] arr=new int[size]; 
+		int[] arr=new int[size];
 		for(int i=0;i<size;i++) {
 			arr[i]=i+1;
 		}
@@ -23,15 +21,11 @@ public class Resizing_In_Arrays {
 		for(int i=0;i<newArr.length;i++) {
 			System.out.println(newArr[i]);
 		}
-
 	}
+	
 	public static int[] resize(int[] a) {
-		int size=a.length;
-		int aa[]=new int[size*2];
-		
-		System.arraycopy(a, 0, aa, 0, size);
-		
+		int aa[]=new int[a.length*2];	
+		System.arraycopy(a, 0, aa, 0, a.length);
 		return aa;
 	}
-
 }
