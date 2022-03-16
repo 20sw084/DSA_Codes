eb76ede0cf2d7d44daf5cd3081e12d1d4de60be4
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-03-16 11:03:09 +0500

    Add files via upload

---
+++ Lec#22/BinaryTree.java
@@ -0,0 +1,55 @@
+
+public class BinaryTree {
+	Object root;
+	BinaryTree left,right;
+	public BinaryTree(Object root, BinaryTree left, BinaryTree right) {
+		super();
+		this.root = root;
+		this.left = left;
+		this.right = right;
+	}
+	public BinaryTree(Object root) {
+		super();
+		this.root = root;
+	}
+	public Object getRoot() {
+		return root;
+	}
+	public void setRoot(Object root) {
+		this.root = root;
+	}
+	public BinaryTree getLeft() {
+		return left;
+	}
+	public void setLeft(BinaryTree left) {
+		this.left = left;
+	}
+	public BinaryTree getRight() {
+		return right;
+	}
+	public void setRight(BinaryTree right) {
+		this.right = right;
+	}
+	@Override
+	public String toString() {
+		StringBuffer buf=new StringBuffer("");
+		if(left!=null) {
+			buf.append(left+",");
+		}
+		buf.append(root);
+		if(right!=null) {
+			buf.append(","+right);
+		}
+		
+		return buf + "";
+	}
+	public static void main(String[] args) {
+		BinaryTree treeB =new BinaryTree("B");
+		BinaryTree treeD =new BinaryTree("D");
+		BinaryTree treeE =new BinaryTree("E");
+		BinaryTree treeC =new BinaryTree("C",treeD,treeE);
+		BinaryTree tree =new BinaryTree("A",treeB,treeC);
+		
+		System.out.println(tree);
+	}
+}
