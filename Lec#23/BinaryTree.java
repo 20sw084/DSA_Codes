341625869ee19e3305f1b99b364a1ad986caa0b5
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-04-04 02:15:49 +0500

    Update BinaryTree.java

--- Lec#23/BinaryTree.java
+++ Lec#23/BinaryTree.java
@@ -1,3 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
 	private Object data;
 	private BinaryTree left,right;
@@ -136,6 +140,134 @@ public class BinaryTree {
     	}
     	return false;
     }
+    
+    public boolean isComplete() {
+    	// return if the tree is empty
+        if (this.getData() == null) {
+            return true;
+        }
+ 
+        // create an empty queue and enqueue the root node
+        Queue<BinaryTree> queue = new ArrayDeque<>();
+        queue.add(this);
+ 
+        // to store the current node
+        BinaryTree front;
+ 
+        // flag to mark the end of full nodes
+        boolean flag = false;
+ 
+        // loop till queue is empty
+        while (!queue.isEmpty())
+        {
+            // dequeue front node
+            front = queue.poll();
+ 
+            // if we have encountered a non-full node before and the current node
+            // is not a leaf, a tree cannot be complete
+            if (flag && (front.left != null || front.right != null)) {
+                return false;
+            }
+ 
+            // if the left child is empty and the right child exists,
+            // a tree cannot be complete
+            if (front.left == null && front.right != null) {
+                return false;
+            }
+ 
+            // if the left child exists, enqueue it
+            if (front.left != null) {
+                queue.add(front.left);
+            }
+            // if the current node is a non-full node, set the flag to true
+            else {
+                flag = true;
+            }
+ 
+            // if the right child exists, enqueue it
+            if (front.right != null) {
+                queue.add(front.right);
+            }
+            // if the current node is a non-full node, set the flag to true
+            else {
+                flag = true;
+            }
+        }
+ 
+        return true;
+    }
+    
+    static int deg=0;
+    
+    public int degree() {
+    	
+    	if(this.getData()==null){
+    		return 0;
+    	}
+    	if(this.getLeft()!=null){
+    		this.getLeft().degree();
+    		deg++;
+    	}
+    	if(this.getRight()!=null){
+    		this.getRight().degree();
+    		deg++;
+    	}
+    	return deg;
+    }
+    
+    public int getNumOfLeafNodes(){
+    	if(this.getData()==null){
+    	return 0;
+    	}
+//    	Initialize an empty Queue
+		Queue<BinaryTree> q  = new LinkedList<>();
+		// Do level order traversal starting from root
+        int count = 0; // Initialize count of leaves
+        q.add(this);
+        while (!q.isEmpty())
+        {
+            BinaryTree temp = q.peek();
+            q.poll();
+ 
+            if (temp.left != null)
+            {
+                q.add(temp.left);
+            }
+            if (temp.right != null)
+            {
+                q.add(temp.right);
+            }
+            if (temp.left == null &&
+                temp.right == null)
+            {
+                count++;
+            }
+        }
+    	return count;
+    }
+    int getLevelUtil(BinaryTree tree, Object data, int level)
+    {
+        if (tree == null)
+            return 0;
+ 
+        if (tree.getData().equals(data))
+            return level;
+ 
+        int downlevel
+            = getLevelUtil(tree.getLeft(), data, level + 1);
+        if (downlevel != 0)
+            return downlevel;
+ 
+        downlevel
+            = getLevelUtil(tree.getRight(), data, level + 1);
+        return downlevel;
+    }
+ 
+    /* Returns level of given data value */
+    int getLevel(Object data)
+    {
+        return getLevelUtil(this, data, 1);
+    }
     // Swap the left and right tree;
  	public void swap() {
  		if(getLeft()==null || getRight()==null) {
@@ -181,22 +313,69 @@ public class BinaryTree {
  		return null;	
  	}
  	
-	public boolean insert(BinaryTree tree,int obj) {
- 		if(tree==null) {
- 			tree.setData(obj);
- 		}
- 		if(obj>(int)tree.getData()) {
- 			insert(tree.getLeft(),obj);
- 		}
- 		if(obj<(int)tree.getData()) {
- 			insert(tree.getRight(),obj);
- 		}
- 		return false;
- 	}
+ 	 // insert a node in BST 
+    void insert(BinaryTree tree,int key)  { 
+    	tree = insert_Recursive(tree, key); 
+    } 
+   
+    //recursive insert function
+    BinaryTree insert_Recursive(BinaryTree root, int key) { 
+          //tree is empty
+        if (root == null) { 
+            root = new BinaryTree(key);
+            return root; 
+        } 
+        //traverse the tree
+        if (key < (int)root.getData())     //insert in the left subtree
+            root.setLeft(insert_Recursive(root.getLeft(), key)); 
+        else if (key > (int)root.getData())    //insert in the right subtree
+            root.setRight(insert_Recursive(root.getRight(), key)); 
+          // return pointer
+        return root; 
+    } 
  	
- 	public boolean delete(BinaryTree tree, int obj) {
- 		return false;
- 	}
+  //delete a node from BST
+    void delete(BinaryTree tree,int key) { 
+        tree = delete_Recursive(tree, key); 
+    } 
+   
+    //recursive delete function
+    BinaryTree delete_Recursive(BinaryTree tree, int key)  { 
+        //tree is empty
+        if (tree == null)  return tree; 
+   
+        //traverse the tree
+        if (key < (int)tree.getData())     //traverse left subtree 
+            tree.setLeft(delete_Recursive(tree.getLeft(), key)); 
+        else if (key > (int)tree.getData())  //traverse right subtree
+            tree.setRight(delete_Recursive(tree.getRight(), key)); 
+        else  { 
+            // node contains only one child
+            if (tree.getLeft() == null) 
+                return tree.getRight(); 
+            else if (tree.getRight() == null) 
+                return tree.getLeft(); 
+   
+            // node has two children; 
+            //get inorder successor (min value in the right subtree) 
+            tree.setData(minValue(tree.getRight())); 
+   
+            // Delete the inorder successor 
+            tree.setRight(delete_Recursive(tree.getRight(), (int)tree.getData())); 
+        } 
+        return tree; 
+    } 
+   
+    int minValue(BinaryTree root)  { 
+        //initially minval = root
+        int minval = (int)root.getData(); 
+        //find minval
+        while (root.getLeft() != null)  { 
+            minval = (int)root.getLeft().getData(); 
+            root = root.getLeft(); 
+        } 
+        return minval; 
+    } 
  	
 	public static void main(String[] args) {
 		BinaryTree treeB =new BinaryTree("B");
@@ -204,15 +383,34 @@ public class BinaryTree {
 		BinaryTree treeE =new BinaryTree("E");
 		BinaryTree treeC =new BinaryTree("C",treeD,treeE);
 		BinaryTree tree =new BinaryTree("A",treeB,treeC);
-		System.out.println(tree.inOrderTraversal());
-		System.out.println(treeC.isLeaf());
-		System.out.println(treeC.size());
-		System.out.println(treeC.contains("C"));
+		System.out.println("Inorder trsversal of tree is : "+tree.inOrderTraversal());
+		System.out.println("Is TreeC have Leaf?  "+treeC.isLeaf());
+		System.out.println("Size of tree is : "+treeC.size());
+		System.out.println("Is treeC contains C?  "+treeC.contains("C"));
+		System.out.println("Left and Right Trees are swapped. ");
 		tree.swap();
-		System.out.println(tree.contains("A"));
-		System.out.println(tree.contains("J"));
-		System.out.println(tree.Right_Most_Of_Left());
-		System.out.println(tree.Left_Most_Of_Right());
-		System.out.println(tree.isFull());
+		System.out.println("Is treeC contains A?  "+tree.contains("A"));
+		System.out.println("Is treeC contains J?  "+tree.contains("J"));
+		System.out.println("Right Most node of left subtree is?  "+tree.Right_Most_Of_Left());
+		System.out.println("Left Most node of right subtree is?  "+tree.Left_Most_Of_Right());
+		System.out.println("Is tree a full binary tree?  "+tree.isFull());
+		System.out.println("Is tree a complete binary tree?  "+tree.isComplete());
+		System.out.println("Level of tree where D is?  "+tree.getLevel("D"));
+		System.out.println("Number of Leaf Nodes of tree is?  "+tree.getNumOfLeafNodes());
+		System.out.println("Degree of tree is : "+tree.degree());
+		
+		BinaryTree treeB2 =new BinaryTree(14);
+		BinaryTree treeD2 =new BinaryTree(18);
+		BinaryTree treeE2 =new BinaryTree(19);
+		BinaryTree treeC2 =new BinaryTree(27,treeD2,treeE2);
+		BinaryTree tree2 =new BinaryTree(15,treeB2,treeC2);
+		System.out.println("Before Insertion, tree is : "+tree2.inOrderTraversal());
+		tree2.insert(tree2, 13);
+		System.out.println("After Insertion, tree is : "+tree2.inOrderTraversal());
+		
+		System.out.println("Before Deletion, tree is : "+tree2.inOrderTraversal());
+		tree2.delete(tree2, 13);
+		System.out.println("After Deletion, tree is : "+tree2.inOrderTraversal());
+
 	}
 }
