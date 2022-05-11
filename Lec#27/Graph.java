6a9bf08a3c4b7ff051627351ea79337758cb3af0
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-05-11 20:47:22 +0500

    Update Graph.java

--- Lec#27/Graph.java
+++ Lec#27/Graph.java
@@ -107,7 +107,6 @@ public class Graph {
 		graph.add("B" , "D");
 		graph.add("C" , "D");
 		graph.add("D" , "E");
-		System.out.println(graph.vertex(2));
 		 int graf[][] = new int[][] { { 0, 0, 1, 2, 0, 0, 0 }, { 0, 0, 2, 0, 0, 3, 0 }, { 1, 2, 0, 1, 3, 0, 0 },
 		        { 2, 0, 1, 0, 0, 0, 1 }, { 0, 0, 3, 0, 0, 2, 0 }, { 0, 3, 0, 0, 2, 0, 1 }, { 0, 0, 0, 1, 0, 1, 0 } };
 		Graph.dijkstra(graf,0);
