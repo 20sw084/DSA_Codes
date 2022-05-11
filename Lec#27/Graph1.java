8bf55718217f48b263612834bbbee19efe51f332
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-05-12 00:04:08 +0500

    Create Graph1.java
    #Copied

---
+++ Lec#27/Graph1.java
@@ -0,0 +1,51 @@
+public class Graph {
+    int size;
+    String[] vertices;
+    boolean a[][];
+    public Graph(String [] args){
+        size=args.length;
+        vertices =new String [size];
+        System.arraycopy(args,0,vertices,0,size);
+        a=new boolean[size][size];
+    }
+    public void add(String v,String w){
+        int i=index(v);
+        int j=index(w);
+        a[i][j]=a[j][i]=true;
+
+    }
+    private int index(String v){
+        for(int i=0;i<size;i++){
+            if(vertices[i].equals(v))
+                return i;
+        }
+        return a.length;
+    }
+    public String toString(){
+        if(size==0) return "{}";
+        StringBuffer buf=new StringBuffer("{"+vertex(0));
+        for(int i=1;i<size;i++)
+            buf.append(","+vertex(i));
+        return buf+"}";
+    }
+    private String vertex(int i){
+        StringBuffer buf =new StringBuffer(vertices[i]+":");
+        for(int j=0;j<size;j++)
+            if(a[i][j])buf.append(vertices[j]);
+            return buf+"";
+
+    }
+
+    public static void main(String[] args) {
+        String[] arg= {"A","B","C","D","E"};
+        Graph graph=new Graph(arg);
+        graph.add("A" , "B");
+        graph.add("A" , "C");
+        graph.add("B" , "C");
+        graph.add("B" , "D");
+        graph.add("C" , "D");
+        graph.add("D" , "E");
+             
+        System.out.println(graph);
+    }
+}
